eed6cc4c5b6e285736d11fc82bf85396d14cf7a7
Hongyang Lyu <s3694521@student.rmit.edu.au>
2019-04-12 11:35:07 +0000

    added full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # According to the page, return the correct title
+  def full_title(page_title = '')
+    base_title = "RAD Assignment 1"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
