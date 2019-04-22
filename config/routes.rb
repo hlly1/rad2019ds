33d85aabd6e70280e8f1792abafb58329fd3b535
Hongyang Lyu <s3694521@student.rmit.edu.au>
2019-04-22 16:23:36 +0000

    Finished sign up

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
   get  '/tab',  to: 'users#index'
+  post '/signup',  to: 'users#create'
   resources :users
 end
