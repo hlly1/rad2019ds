2f21c32e05871a52bc855506cee7c25120c41f04
Hongyang Lyu <s3694521@student.rmit.edu.au>
2019-04-27 09:43:24 +0000

    Fix the error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { minimum: 4 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@\b(?i)rmit.edu.au\b/
   validates :email, presence: true, length: { minimum: 4 },
-                    format: { with: VALID_EMAIL_REGEX },
+                    format: { with: VALID_EMAIL_REGEX, :message => "Registration only open for RMIT staff"},
                     uniqueness: { case_sensitive: false }
   has_secure_password
   
