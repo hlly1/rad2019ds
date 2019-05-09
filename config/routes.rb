1ed3c07fbcd297bfca1adab5390c5af1f1cc62dc
Dan Wang <s3617881@student.rmit.edu.au>
2019-05-09 05:23:18 +0000

    added error message for cate and locations having no courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
   get  '/allcourse', to:  'static_pages#index'
-
   get '/categories/:name', to: 'categories#show', as: 'categories'
   get '/locations/:id', to: 'locations#show', as: 'locations'
+  
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
   get  '/addlocation',  to: 'locations#new'
   post '/addlocation',  to: 'locations#create'
   get  '/addcategory',  to: 'categories#new'
