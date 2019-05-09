52922b81f27014aa5a0a5c064bbb22328ad2b6db
Dan Wang <s3617881@student.rmit.edu.au>
2019-05-09 05:43:01 +0000

    start course creating

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   
+  get  '/newcourse',  to: 'courses#new'
+  post '/newcourse',  to: 'courses#create'
+  
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
