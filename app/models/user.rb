657937b3df3f3ee0e72d943402f7e7fb7ea51054
Hongyang Lyu <s3694521@student.rmit.edu.au>
2019-05-19 05:04:22 +0000

    disable validation for admin to register

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,19 +5,19 @@ class User < ApplicationRecord
     self.email = self.email.downcase
   }
 
-  validates :name,  presence: true, length: { minimum: 4 }
-  VALID_EMAIL_REGEX = /[A-Za-z_0-9]+\.+[A-Za-z_0-9]+@\b(?i)rmit.edu.au\b/
-  validates :email, presence: true, length: { minimum: 4 },
-                    format: { with: VALID_EMAIL_REGEX, :message => "Registration only open for RMIT staff"},
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  VALID_PASSWD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!~]).{8,}$/
+  # validates :name,  presence: true, length: { minimum: 4 }
+  # VALID_EMAIL_REGEX = /[A-Za-z_0-9]+\.+[A-Za-z_0-9]+@\b(?i)rmit.edu.au\b/
+  # validates :email, presence: true, length: { minimum: 4 },
+  #                   format: { with: VALID_EMAIL_REGEX, :message => "Registration only open for RMIT staff"},
+  #                   uniqueness: { case_sensitive: false }
+  # has_secure_password
+  # VALID_PASSWD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$&+,:;=?@#|'<>.^*()%!~]).{8,}$/
 
-  validates :password, presence: true,
-                       format: { with: VALID_PASSWD_REGEX, :message => 
-                       "must contains at least a lowercase letter, a uppercase,
-                       a digit, a special character and 8+ characters", :multiline => true}
-                       #allow_nil: true
+  # validates :password, presence: true,
+  #                     format: { with: VALID_PASSWD_REGEX, :message => 
+  #                     "must contains at least a lowercase letter, a uppercase,
+  #                     a digit, a special character and 8+ characters", :multiline => true}
+                       #allow_nil: true 
   
   
   def User.digest(string)
